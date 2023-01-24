7991c5dca09fd0a21efdbeb51d7c1ad8647988a1
Sofia Lumme <sofia.lumme@gmail.com>
2023-01-24 02:51:34 +0200

    tehtävä valmis

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [no1, setNo1] = useState(0);
+  const [no2, setNo2] = useState(0);
+  const [result, setResult] = useState(0);
+
+  const buttonPressed = (title) => {
+    if (title == '+') {
+      setResult(parseInt(no1) + parseInt(no2));
+    } else {
+      setResult(parseInt(no1) - parseInt(no2));
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Result is {result}</Text>
+      <TextInput
+        style={{ width: 200, borderColor: 'gray', borderWidth: 1 }}
+        keyboardType='numeric'
+        onChangeText={no1 => setNo1(no1)}
+        value={no1}
+      />
+      <TextInput
+        style={{ width: 200, borderColor: 'gray', borderWidth: 1 }}
+        keyboardType='numeric'
+        onChangeText={no2 => setNo2(no2)}
+        value={no2}
+      />
+      <View style={{ flexDirection: 'row'}}>
+      <Button title='+' onPress={ () => buttonPressed('+')} />
+      <Button title='-' onPress={ () => buttonPressed('-')} />
+      </View>
     </View>
   );
 }
